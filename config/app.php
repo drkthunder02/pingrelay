08706ee6209838a860e2f3435c34c0ec3d326c94
drkthunder02 <mancuso.christopher@gmail.com>
2019-02-04 00:15:50 -0600

    collective added

--- config/app.php
+++ config/app.php
@@ -228,8 +228,8 @@ return [
 
         'Entrust' => Zizaco\Entrust\EntrustFacade::class,
         'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,       
-
+        'Html' => Collective\Html\HtmlFacade::class,
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
     ],
 
 ];
